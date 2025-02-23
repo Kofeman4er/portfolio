49eb37a650df381e9acadf81fdd0f3fe7fdd51af
Kofeman4er <48658085+Kofeman4er@users.noreply.github.com>
2025-02-23 00:49:27 -0700

    first commit

---
+++ data/index.ts
@@ -0,0 +1,202 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Approach", link: "#approach" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "We prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "We are very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "Our tech stack",
+      description: "We are constantly developing",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Team of enthusiasts with a passion for development",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a JS Animation library",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Freelancer mechanic website",
+      des: "A user-friendly website connecting vehicle owners with skilled freelancer mechanic",
+      img: "/p1.svg",
+      iconLists: ["/re.svg", "/tail.svg",  "/three.svg", "/fm.svg"],
+      link: "https://mechanic-portfolio-de9il457a-romans-projects-9c8eee0b.vercel.app/",
+    },
+    {
+      id: 2,
+      title: "In progress",
+      des: "Simplify your video conferencing experience. Seamlessly connect with colleagues and friends.",
+      img: "/p2.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Collaborating with Keen dev was an absolute pleasure. Their professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Keen devs enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Keen dev is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Keen dev was an absolute pleasure. Their professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Keen devs enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Keen dev is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Keen dev was an absolute pleasure. Their professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Keen devs enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Keen dev is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Keen dev was an absolute pleasure. Their professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Keen devs enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Keen dev is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Keen dev was an absolute pleasure. Their professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Keen devs enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Keen dev is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Backend Development",
+      desc: "Building robust and scalable server-side architectures that power dynamic applications. Our backend solutions focus on security, performance, and efficiency, using technologies like Node.js, Express, and Django.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Design",
+      desc: "Transforming ideas into captivating digital experiences with intuitive and user-friendly design. From wireframes to polished UI/UX, we prioritize aesthetics, usability, and brand identity to create visually engaging websites.",
+      className: "md:col-span-2", 
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Databases",
+      desc: "Structuring and managing data with powerful database solutions like PostgreSQL, MongoDB, and MySQL. We optimize storage, retrieval, and security to ensure smooth and reliable data management for your applications.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Frontend Development",
+      desc: "Crafting visually stunning and highly interactive user interfaces with modern frameworks like React and Next.js. We ensure seamless navigation, responsive design, and an exceptional user experience across all devices.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
