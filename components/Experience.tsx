fd093aab7c0f15814f8a699bed06eceda86ac4f6
Kofeman4er <48658085+Kofeman4er@users.noreply.github.com>
2025-02-23 12:26:32 -0700

    5

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,6 +1,7 @@
 import { workExperience } from '@/data'
 import React from 'react'
 import { Button } from './ui/MovingBorders'
+import Image from 'next/image'
 
 const Experience = () => {
     return (
@@ -11,20 +12,27 @@ const Experience = () => {
             </h1>
 
             <div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10">
-                {workExperience.map((card)=>(
+                {workExperience.map((card) => (
                     <Button
-                    key={card.id}
-                    duration={Math.floor(Math.random() * 10000 + 10000)}
-                    borderRadius='1.75rem'
-                    className='flex-1 text-white border-neutral-200 dark:border-slate-800'
+                        key={card.id}
+                        duration={Math.floor(Math.random() * 10000 + 10000)}
+                        borderRadius="1.75rem"
+                        className="flex-1 text-white border-neutral-200 dark:border-slate-800"
                     >
-                        <div className='flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2'>
-                            <img src={card.thumbnail} alt={card.thumbnail} className='lg:w-32 md:w-20 w-16' />
-                            <div className='lg:ms-5'>
-                                <h1 className='text-start text-xl md:text-2xl font-bold'>
+                        <div className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2">
+                            {/* Use next/image for image optimization */}
+                            <Image
+                                src={card.thumbnail}
+                                alt={card.title} // Use a descriptive alt text
+                                width={128} // Set the width and height
+                                height={128} // Use appropriate dimensions based on your design
+                                className="lg:w-32 md:w-20 w-16"
+                            />
+                            <div className="lg:ms-5">
+                                <h1 className="text-start text-xl md:text-2xl font-bold">
                                     {card.title}
                                 </h1>
-                                <p className='text-start text-white-100 mt-3 font-semibold'>
+                                <p className="text-start text-white-100 mt-3 font-semibold">
                                     {card.desc}
                                 </p>
                             </div>
